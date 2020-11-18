3e46e5f94148194e8329328e9689bca4db19fb63
Nathanael <nathanael.meshesha@yahoo.com>
2020-11-18 05:27:53 -0600

    Added presence of true validations to our Customer model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
   belongs_to :province
-
+  validates :full_name, :email, :password, :postal_code, :billing_address, :shipping_address, :city, :country, :phone_number, presence: true
 end
