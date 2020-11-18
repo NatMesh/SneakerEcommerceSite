d4ceb156c0e31fb6cbe54e79a314a6235d067158
Nathanael <nathanael.meshesha@yahoo.com>
2020-11-18 05:08:08 -0600

    Updated our Customer model to have a postal_code column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_102839) do
+ActiveRecord::Schema.define(version: 2020_11_18_110531) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "password"
+    t.string "billing_address"
+    t.string "shipping_address"
+    t.string "city"
+    t.integer "province_id", null: false
+    t.string "country"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "postal_code"
+    t.index ["province_id"], name: "index_customers_on_province_id"
+  end
 
   create_table "provinces", force: :cascade do |t|
     t.string "name"
@@ -20,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_11_18_102839) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "customers", "provinces"
 end
