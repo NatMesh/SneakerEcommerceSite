97fd4376058e20ecd2a38e00ec65577cc2d4d004
Nathanael <nathanael.meshesha@yahoo.com>
2020-11-24 19:09:27 -0600

    Changed price column in order_items table from decimal to float for better accuracy with dollar values.

---
+++ db/migrate/20201125010754_change_price_type_in_order_items.rb
@@ -0,0 +1,5 @@
+class ChangePriceTypeInOrderItems < ActiveRecord::Migration[6.0]
+  def change
+    change_column :order_items, :price, :decimal
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_005911) do
+ActiveRecord::Schema.define(version: 2020_11_25_010754) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_005911) do
   create_table "order_items", force: :cascade do |t|
     t.integer "sneaker_id", null: false
     t.integer "order_id", null: false
-    t.float "price"
+    t.decimal "price"
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
