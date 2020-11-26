7b5e9828ae5549c9db020dd1255e44c3a492707a
Nathanael <nathanael.meshesha@yahoo.com>
2020-11-26 17:08:07 -0600

    Generated controller for Categories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'brands/index'
-  get 'brands/show'
-  get 'sneakers/index'
-  get 'sneakers/show'
+  get 'categories/index'
+  get 'categories/show'
+  resources :sneakers, only: %i[index show]
+  resources :brands, only: %i[index show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
