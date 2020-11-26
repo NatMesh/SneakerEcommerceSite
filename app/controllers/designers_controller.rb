c5e44a7cf05186368c5218ddafb2a61c5ce1eb75
Nathanael <nathanael.meshesha@yahoo.com>
2020-11-26 17:31:45 -0600

    Generated a controller for Designers.

---
+++ app/controllers/designers_controller.rb
@@ -0,0 +1,7 @@
+class DesignersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
   resources :sneakers, only: %i[index show]
   resources :brands, only: %i[index show]
+  resources :designers, only: %i[index show]
+  resources :categories, only: %i[index show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
