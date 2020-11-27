57ebd12309878a95482c029a2bf09d9a0ec7304f
Nathanael <nathanael.meshesha@yahoo.com>
2020-11-27 02:59:01 -0600

    added content to sneakers index page and links to navigate to sneakers show page.

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,7 +1,16 @@
 class SneakersController < ApplicationController
   def index
+    #This includes statement is very important as to avoid "N+1" errors when traversing models to retrieve data
+    #Limits the amount of times we hit the database
+    @sneakers = Sneaker.includes(:brand, :designer, :category).order("price_cents DESC")
   end
+  #The @sneakers var will be shared with:
+  #app/views/sneakers/index.html.erb
 
   def show
+    #fetches us all the data for a particular sneaker based on the id
+    @sneaker = Sneaker.find(params[:id])
   end
+  #The @sneaker variable will be shared with:
+  #app/views/sneakers/show.html.erb
 end
