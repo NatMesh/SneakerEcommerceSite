ead2f52aa1103c212697fcc80978a67e0f8ecdee
Nathanael <nathanael.meshesha@yahoo.com>
2020-12-02 14:27:56 -0600

    Added form fields for user sign in and user account edit page in devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :province_id, :address, :country, :city, :postal_code, :phone_number) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :province_id, :address, :country, :city, :postal_code, :phone_number) }
+  end
 end
