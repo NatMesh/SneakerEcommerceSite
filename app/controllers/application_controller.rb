d2ed5f17c3e680d51724b20f2f02c37adc95c243
Nathanael <nathanael.meshesha@yahoo.com>
2020-12-02 15:26:56 -0600

    updated application view to look for if customer is signed in over old version using user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password, :province_id, :address, :country, :city, :postal_code, :phone_number) }
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :province_id, :address, :country, :city, :postal_code, :phone_number) }
   end
+
 end
