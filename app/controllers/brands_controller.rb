b4331d63e0088a758545ddd98508d437657f07f6
Nathanael <nathanael.meshesha@yahoo.com>
2020-12-02 03:37:56 -0600

    Users can now view shoes based on brand(Nike, Vans, Jordan, etc) and show page has pagination.

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,5 +4,8 @@ class BrandsController < ApplicationController
   end
 
   def show
+    #Fetches all the sneakers that belong to the brand we chose
+    @brand = Brand.find(params[:id])
+    @sneakers = Brand.find(params[:id]).sneakers.page params[:page]
   end
 end
