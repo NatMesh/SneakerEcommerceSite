8b22edd73c403d8c98795ac6a20e294128132dd2
Nathanael <nathanael.meshesha@yahoo.com>
2020-12-02 04:58:39 -0600

    Added desginer show page, index page and way for users to find shoes based on designer.

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,7 +1,11 @@
 class DesignersController < ApplicationController
   def index
+    @designers = Designer.all
   end
 
   def show
+    #Fetches all the sneakers that belong to the designer we chose
+    @designer = Designer.find(params[:id])
+    @sneakers = Designer.find(params[:id]).sneakers.page params[:page]
   end
 end
--- app/models/designer.rb
+++ app/models/designer.rb
@@ -1,4 +1,5 @@
 class Designer < ApplicationRecord
   has_many :sneakers
   validates :name, uniqueness: true
+  paginates_per 20
 end
