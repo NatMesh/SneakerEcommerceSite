200ce6914aa397f4d3048cbd868753d6c93cf0eb
Nathanael <nathanael.meshesha@yahoo.com>
2020-12-09 13:48:00 -0600

    Created a cart controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,27 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password, :province_id, :address, :country, :city, :postal_code, :phone_number) }
   end
 
+
+
+  before_action :initialize_session
+  helper_method :cart
+  helper_method :sneaker_categories
+
+  private
+
+
+  def sneaker_categories
+    #retrieves all product categories and lets them be available on all pages
+    Category.all
+  end
+
+  def initialize_session
+    #This is where we initialize our shopping cart.
+    session[:shopping_cart] ||= [] #The shopping cart will be an array of product ids.
+  end
+
+  def cart
+    #Return a collection of Product objects based on the product ids in the shopping cart.
+    Sneaker.find(session[:shopping_cart])
+  end
 end
--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,6 @@
 class CheckoutController < ApplicationController
+  #POST /checkout/create
+  #A product id will be in the params hash: params[:product_id]
   def create
     #Establish a connection with Stripe and then redirect the user to the payment screen.
   end
