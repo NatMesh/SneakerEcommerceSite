6a97b08350d6032195e022672c14b679ace36f86
Nathanael <nathanael.meshesha@yahoo.com>
2020-12-09 07:50:25 -0600

    Created a new checkout controller with associated routes.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,13 @@
 class CheckoutController < ApplicationController
+  def create
+    #Establish a connection with Stripe and then redirect the user to the payment screen.
+  end
+
+  def success
+    #We tooke the customers money!
+  end
+
+  def cancel
+    #Something went wrong with the payment!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,13 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #for our routes relating to stripe payments being created, succeeding or failing we will use scoped routes
+  #A way of defining routes that start with the same sub component in this case /checkout/
+  #this is an alt to restful routing because the routes we will use here are outside of the normal 7 RESTful routes
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
+
 end
